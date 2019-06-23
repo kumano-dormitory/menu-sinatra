5cc02589921e98765275a5945c07fa8d3335f6b7
bo40 <bo40.git@gmail.com>
2019-06-23 19:55:44 +0900

    add json actions

--- server.rb
+++ server.rb
@@ -8,6 +8,13 @@ get '/menus' do
                        ).order("day ASC, id DESC").group(:day)
   erb :index, layout: :application
 end
+get '/menus.json' do
+  @menus = Menu.where("day >= :start AND day <= :end",
+                        {start: (Date.today + 2).beginning_of_week, end: (Date.today + 2).end_of_week}
+                       ).order("day ASC, id DESC").group(:day)
+  obj = @menus.map{|menu| {id: menu.id, day: menu.day, lunch1: menu.lunch1, lunch2: menu.lunch2, dinner: menu.dinner, url: "https://menus.kumano-ryo.com/menus/#{menu.id}.json"}}
+  JSON.pretty_generate(obj)
+end
 
 # new
 get '/menus/new' do
@@ -15,6 +22,10 @@ get '/menus/new' do
   erb :new, layout: :application
 end
 
+get '/menus/*.json' do |id|
+  @menu = Menu.find(id)
+  JSON.pretty_generate({id: @menu.id, day: @menu.day, lunch1: @menu.lunch1, lunch2: @menu.lunch2, dinner: @menu.dinner, created_at: @menu.created_at, updated_at: @menu.updated_at})
+end
 # show
 get '/menus/:id' do
   @menu = Menu.find(params[:id])
@@ -36,8 +47,28 @@ post '/menus' do
     erb :create, layout: :application
   end
 end
+post '/menus.json' do
+  @menu = Menu.new(params.slice(:day, :lunch1, :lunch2, :dinner))
+  if @menu.save
+    status 201
+    body JSON.pretty_generate({id: @menu.id, day: @menu.day, lunch1: @menu.lunch1, lunch2: @menu.lunch2, dinner: @menu.dinner, created_at: @menu.created_at, updated_at: @menu.updated_at})
+  else
+    status 422
+    body JSON.pretty_generate(@menu.errors)
+  end
+end
 
 # update
+patch '/menus/*.json' do |id|
+  @menu = Menu.find(id)
+  if @menu.update(params.slice(:day, :lunch, :lunch2, :dinner))
+    status 200
+    body JSON.pretty_generate({id: @menu.id, day: @menu.day, lunch1: @menu.lunch1, lunch2: @menu.lunch2, dinner: @menu.dinner, created_at: @menu.created_at, updated_at: @menu.updated_at})
+  else
+    status 422
+    body JSON.pretty_generate(@menu.errors)
+  end
+end
 patch '/menus/:id' do
   @menu = Menu.find(params[:id])
   if @menu.update(params.slice(:day, :lunch1, :lunch2, :dinner))
