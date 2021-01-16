f92de5dee2a75cf67ac13a7499e173ca7e20a0c8
bo40 <bo40.git@gmail.com>
2021-01-16 14:53:40 +0900

    change db path

--- model/menu.rb
+++ model/menu.rb
@@ -1,4 +1,4 @@
 require 'sinatra/activerecord'
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'menus.sqlite3')
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: '/var/db/menus.sqlite3')
 class Menu < ActiveRecord::Base; end
